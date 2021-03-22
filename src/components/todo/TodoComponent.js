ec849c810fa4c472b5db8204ef92af9eb33483c2
mynspluto <mynspluto@gmail.com>
2021-03-22 14:02:36 +0900

    .

--- src/components/todo/TodoComponent.js
+++ src/components/todo/TodoComponent.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-import {Formik, Form, Field} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import TodoDataService from '../../api/todo/TodoDataService.js'
+import AuthenticationService from './AuthenticationService.js'
 
 class TodoComponent extends Component {
 
@@ -9,13 +11,38 @@ class TodoComponent extends Component {
     super(props)
 
     this.state = {
-      id: 1,
-      description: "Learn Forms",
+      id: this.props.match.params.id,
+      description: '',
       targetDate: moment(new Date()).format('YYYY-MM-DD')
 
     }
 
     this.onSubmit = this.onSubmit.bind(this)
+    this.validate = this.validate.bind(this)
+  }
+
+  componentDidMount() {
+    let username = AuthenticationService.getLoggedInUserName()
+    TodoDataService.retrieveTodo(username, this.state.id)
+      .then(response => this.setState({
+        description: response.data.description,
+        targetDate: moment(response.data.targetDate).format('YYYY-MM-DD')
+      }))
+      
+  }
+
+  validate(values) {
+    let errors = {}
+    if(!values.description) {
+      errors.description = 'Enter a Description'
+    }
+    else if(values.description.length<5) {
+      errors.description = 'Enter atleast 5 Characters in Description'
+    }
+
+    if(values.targetDate)
+    
+    return errors
   }
 
   onSubmit(values) {
@@ -32,10 +59,15 @@ class TodoComponent extends Component {
           <Formik
             initialValues={{description, targetDate}}
             onSubmit={this.onSubmit}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validate={this.validate}
+            enableReinitialize={true}
           >
             {
               (props) => (
                 <Form>
+                  <ErrorMessage name="description" component="div" className="alert alert-warning"/>
                   <fieldset className="form-group">
                     <label>Description</label>
                     <Field className="form-control" type="text" name="description"/> 
